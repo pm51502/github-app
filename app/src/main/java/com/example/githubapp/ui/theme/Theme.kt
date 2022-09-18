df9660d753f157b8e9ba55fff8a91fbc850ac0a7
Petar Milicevic <pm51502@fer.hr>
2022-09-18 23:29:40 +0200

    viewmodel fix

--- app/src/main/java/com/example/githubapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/githubapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.githubapp.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -28,12 +27,8 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun GitHubAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun GitHubAppTheme(content: @Composable () -> Unit) {
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
